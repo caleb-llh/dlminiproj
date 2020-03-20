e75b8c6957066adb4cd2923aa86527b0a9c0d858
caleb-llh <caleb_lee@mymail.sutd.edu.sg>
2020-03-20 22:54:07 +0800

    added get_dataset shell script

---
+++ get_dataset.sh
@@ -0,0 +1,3 @@
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1gxFVai1Ek4JyST2IrKvtA-j68cFiZgFR' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1gxFVai1Ek4JyST2IrKvtA-j68cFiZgFR" -O "pascal-voc-2012.zip" && rm -rf /tmp/cookies.txt
+unzip "pascal-voc-2012.zip"
+rm "pascal-voc-2012.zip"
