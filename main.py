cda985a7a57cdd4f8a0ea0e6410b6dcefcb2c0e6
caleb-llh <cllh1996@gmail.com>
2020-03-21 12:10:01 +0800

    from local

--- main.py
+++ main.py
@@ -139,6 +139,8 @@ def results(device, loadervl, validset):
     ## Get class-wise average precision and mean average precision
     class_precision, ave_precision = utils.evaluate(model, loadervl, device)
     print("Class-wise average precision")
+    print(validset.list_image_sets())
+    print(class_precision)
     for i in range(len(class_precision)):
         print("{}: {}".format(validset.list_image_sets()[i]),class_precision[i])
     print("Mean average precision: {}".format(ave_precision))
